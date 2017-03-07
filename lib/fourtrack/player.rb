0508df1f6178c1bac59501ef4c4bb8ac0a7ef00a
Julik Tarkhanov <me@julik.nl>
2017-03-07 18:08:02 +0100

    Note a horrible inefficiency

--- lib/fourtrack/player.rb
+++ lib/fourtrack/player.rb
@@ -14,6 +14,12 @@ class Fourtrack::Player
       break if @io.eof?
       zr = Zlib::GzipReader.new(@io)
       zr.each_line(&blk)
+      # TODO:
+      # this basically allocates a GIANT string if the file is big
+      # and the amount of data remaining is substantial.
+      # See
+      # https://github.com/ruby/ruby/blob/0adce993578ca4c40afbbc04c5f4679561bd7861/ext/zlib/zlib.c#L2948
+      # Something different is needed - maybe even streaming from commandline gunzip...
       unused_bytestr = zr.unused
       zr.finish
       if unused_bytestr && unused_bytestr.bytesize.nonzero?
